c8b69c43823d37445be4426d69b10ca157aaddc2
Shivam Kumar <kumarshivam7758@gmail.com>
2025-09-18 00:21:08 +0530

    Landind Page

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change navbar style on scroll
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
